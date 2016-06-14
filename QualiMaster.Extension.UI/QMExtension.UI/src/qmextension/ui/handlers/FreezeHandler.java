e06acff319c6fa0ea2df550c217cbaa4053e5318
Elscha <elscha@sse.uni-hildesheim.de>
2016-06-14 07:24:43 +0200

    Fixed comments

--- QualiMaster.Extension.UI/QMExtension.UI/src/qmextension/ui/handlers/FreezeHandler.java
+++ QualiMaster.Extension.UI/QMExtension.UI/src/qmextension/ui/handlers/FreezeHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2016 University of Hildesheim, Software Systems Engineering
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package qmextension.ui.handlers;
 
 import java.util.ArrayList;
@@ -31,11 +46,10 @@ import net.ssehub.easy.varModel.model.filter.FilterType;
 import net.ssehub.easy.varModel.model.rewrite.ProjectRewriteVisitor;
 
 /**
- * Our sample handler extends AbstractHandler, an IHandler base class.
- * @see org.eclipse.core.commands.IHandler
- * @see org.eclipse.core.commands.AbstractHandler
+ * Freezes the QM model as it is done in the QM tool.
+ * @author El-Sharkawy
  */
-public class FreezeHandler  implements IObjectActionDelegate {
+public class FreezeHandler implements IObjectActionDelegate {
 	
     private Shell shell;
     private ISelection selection;
